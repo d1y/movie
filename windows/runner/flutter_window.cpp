154d3ccb35478aba1fb66615e18fa22253128c0c
d1y <chenhonzhou@gmail.com>
2024-06-21 08:02:22 +0800

    windows: ensure the window is shown
    https://docs.flutter.dev/release/breaking-changes/windows-show-window-migration

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -26,6 +26,11 @@ bool FlutterWindow::OnCreate() {
   }
   RegisterPlugins(flutter_controller_->engine());
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
+  // Flutter can complete the first frame before the "show window" callback is
+  // registered. The following call ensures a frame is pending to ensure the
+  // window is shown. It is a no-op if the first frame hasn't completed yet.
+  // ref https://docs.flutter.dev/release/breaking-changes/windows-show-window-migration
+  flutter_controller_->ForceRedraw();
   return true;
 }
 
